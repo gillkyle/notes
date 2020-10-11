964f4a9ac689b5cc93fe64acd175552bf04cd894
Kyle Gill <kylerobertgill@gmail.com>
2020-10-11 10:42:13 -0600

    reformat graph overview

--- content/minimalism.md
+++
@@ -1,5 +0,0 @@
----
-title: 'Minimalism'
----
-
-Minimalism isn't about doing less, it's about doing more of the things that are most important.
--- src/pages/overview.js
+++ src/pages/overview.js
@@ -6,22 +6,31 @@ import Header from '../gatsby-theme-andy/components/Header';
 import '../style.css';
 
 const Overview = ({ data }) => {
-  console.log(data);
   return (
     <div className="text-gray-900 flex flex-col min-h-screen h-screen">
       <Header siteMetadata={data.site.siteMetadata} />
-      <div className="note-container px-4 overflow-y-auto bg-white md:sticky flex flex-col flex-shrink-0">
+      <div className="note-container p-8 overflow-y-auto bg-white md:sticky flex flex-col flex-shrink-0">
         <h1>Graph Overview</h1>
         <p className="my-4">
           All pages are listed here, those that are in lowercase have yet to be written about
           themselves (though I intend to):
         </p>
-        {data.brain.nodes.map((node) => (
-          <div className="py-1">
-            <Link to={`/${node.slug}`}>{node.title}</Link>: ({node.inboundReferences.length}{' '}
-            reference{!!node.inboundReferences.length && `s`})
-          </div>
-        ))}
+        <div
+          className="grid gap-4"
+          style={{ gridTemplateColumns: `repeat(auto-fit,minmax(160px,1fr))` }}
+        >
+          {data.brain.nodes.map((node) => (
+            <Link
+              to={`/${node.slug}`}
+              className="rounded p-2 transition duration-100 hover:bg-blue-100 no-underline border-solid"
+            >
+              <div className="">
+                <h2 className="text-lg">{node.title}</h2>({node.inboundReferences.length} reference
+                {!!node.inboundReferences.length && `s`})
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
